1fc08c0fe32b4b52aa13dfc3ce67f8048aee2fdd
unknown <lassehovden@gmail.com>
2019-09-08 10:28:38 +0200

    running test 1 to 5 Roman

---
+++ src/test/java/RomanNumerals.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanNumerals {
+
+  @Test
+  void Conver1toI(){
+   assertEquals("I",toRoman(1));
+  }
+
+  @Test
+  void Conver2toII(){
+   assertEquals("II",toRoman(2));
+  }
+
+  @Test
+  void Conver3toIII(){
+   assertEquals("III",toRoman(3));
+  }
+
+  @Test
+  void Conver4toIV(){
+    assertEquals("IV",toRoman(4));
+  }
+
+  @Test
+  void Conver5toV(){
+    assertEquals("V",toRoman(5));
+  }
+
+  private String toRoman(int number){
+String inputnumber = "";
+
+    if (number == 5){inputnumber="V";}
+    else
+    if (number == 4){inputnumber="IV";}
+    else
+    for (int i = 1; i <= number ; i++) {
+      inputnumber += "I";
+
+    }
+
+    return inputnumber;
+  }
+
+
+
+
+}
