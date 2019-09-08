ccab6ec4dd960c32115818eb71628f270490efaf
LASSE <lassehovden@gmail.com>
2019-09-08 14:43:37 +0200

    added simple stest for bigger convert

--- src/test/java/no/kristiania/RomanNumerals/RomanNumeralsTest.java
+++ src/test/java/no/kristiania/RomanNumerals/RomanNumeralsTest.java
@@ -6,36 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralsTest {
 
-  @Test
-  void Conver1toI(){
-   assertEquals("I", romanConverter.toRoman(1));
-  }
-  @Test
-  void Conver2toII(){
-   assertEquals("II", romanConverter.toRoman(2));
-  }
-  @Test
-  void Conver3toIII(){
-    assertEquals("III", romanConverter.toRoman(3));
-  }
+
   @Test
   void Conver4toIV(){
     assertEquals("IV", romanConverter.toRoman(4));
   }
-  @Test  void Conver5toV(){
+
+  @Test
+  void Conver5toV(){
     assertEquals("V", romanConverter.toRoman(5));
-  }
-  @Test
-  void Conver6toVI(){
-    assertEquals("VI", romanConverter.toRoman(6));
-  }
-  @Test
-  void Conver7toVII(){
-    assertEquals("VII", romanConverter.toRoman(7));
-  }
-  @Test
-  void Conver8toVIII(){
-    assertEquals("VIII", romanConverter.toRoman(8));
   }
 
   @Test
@@ -44,9 +23,11 @@ public class RomanNumeralsTest {
   }
 
   @Test
-  void Conver10toX(){
-    assertEquals("X", romanConverter.toRoman(10));
+  void convertTestSimple(){
+    assertEquals("MCMXCIX", romanConverter.toRoman(1999));
   }
+
+
 
 
 }
