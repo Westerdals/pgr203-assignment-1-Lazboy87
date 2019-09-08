b8d5eff0b45aa45a3d7d3e1fff39c9604c76405f
Fredrik Holanger <fholange@gmail.com>
2019-09-08 14:41:42 +0200

    Added all roman symbols

--- src/main/java/no/kristiania/RomanNumerals/romanConverter.java
+++ src/main/java/no/kristiania/RomanNumerals/romanConverter.java
@@ -8,6 +8,14 @@ public class romanConverter {
       StringBuilder result= new StringBuilder();
 
 
+    number = toRomanDigit(number, result,1000,"M");
+    number = toRomanDigit(number, result,900,"CM");
+    number = toRomanDigit(number, result,500,"D");
+    number = toRomanDigit(number, result,400,"CD");
+    number = toRomanDigit(number, result,100,"C");
+    number = toRomanDigit(number, result,90,"XC");
+    number = toRomanDigit(number, result,50,"L");
+    number = toRomanDigit(number, result,40,"XL");
     number = toRomanDigit(number, result,10,"X");
     number = toRomanDigit(number, result,9,"IX");
     number = toRomanDigit(number, result,5,"V");
