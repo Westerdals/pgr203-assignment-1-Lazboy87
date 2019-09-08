d0e27f21fcb47a43b59f25e9db45f3344d907433
unknown <lassehovden@gmail.com>
2019-09-08 11:23:12 +0200

    added test 1 to 5 roman numbers

--- src/test/java/RomanNumerals.java
+++ src/test/java/RomanNumerals.java
@@ -8,15 +8,43 @@ public class RomanNumerals {
   void Conver1toI(){
    assertEquals("I",toRoman(1));
   }
+  @Test
+  void Conver2toII(){
+   assertEquals("II",toRoman(2));
+  }
+  @Test
+  void Conver3toIII(){
+    assertEquals("III",toRoman(3));
+  }
+  @Test
+  void Conver4toIV(){
+    assertEquals("IV",toRoman(4));
+  }
+  @Test
+  void Conver5toV(){
+    assertEquals("V",toRoman(5));
+  }
+
 
 
 
   private String toRoman(int number){
+    String inputNumber="";
+    if (number==5){inputNumber="V";}
+    else
+    if (number==4){inputNumber="IV";}
+    else
+      for (int i = 1; i <=number ; i++) {
+        inputNumber  +="I";
+
+      }
 
 
 
 
-    return "I";
+
+
+    return inputNumber;
   }
 
 
